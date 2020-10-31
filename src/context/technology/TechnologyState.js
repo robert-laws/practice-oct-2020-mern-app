8ab06deade8816a4ae7b48e1cf38c57cad9b6f57
Robert Laws <roblaws@me.com>
2020-10-31 14:49:39 +0300

    update technology context

--- src/context/technology/TechnologyState.js
+++ src/context/technology/TechnologyState.js
@@ -1,7 +1,7 @@
 import React, { useReducer, useCallback } from 'react';
 import TechnologyContext from './technologyContext';
 import technologyReducer from './technologyReducer';
-import { GET_ALL_DATA } from '../types';
+import { GET_ITEMS, ADD_ITEM, UPDATE_ITEM, DELETE_ITEM } from '../types';
 import data from '../../data/allData';
 
 const TechnologyState = ({ children }) => {
@@ -13,17 +13,53 @@ const TechnologyState = ({ children }) => {
 
   const getAllTechnologyData = useCallback(() => {
     try {
-      dispatch({ type: GET_ALL_DATA, payload: data });
+      dispatch({ type: GET_ITEMS, payload: data });
     } catch (error) {
       console.log(error);
     }
   }, [dispatch]);
+
+  const addItem = useCallback(
+    (item) => {
+      try {
+        dispatch({ type: ADD_ITEM, payload: item });
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    [dispatch]
+  );
+
+  const updateItem = useCallback(
+    (id, item) => {
+      try {
+        dispatch({ type: UPDATE_ITEM, payload: item });
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    [dispatch]
+  );
+
+  const deleteItem = useCallback(
+    (id) => {
+      try {
+        dispatch({ type: DELETE_ITEM, payload: id });
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    [dispatch]
+  );
 
   return (
     <TechnologyContext.Provider
       value={{
         technologyData: state.technologyData,
         getAllTechnologyData,
+        addItem,
+        updateItem,
+        deleteItem,
       }}
     >
       {children}
--- src/context/technology/technologyReducer.js
+++ src/context/technology/technologyReducer.js
@@ -1,11 +1,31 @@
-import { GET_ALL_DATA } from '../types';
+import { GET_ITEMS, ADD_ITEM, UPDATE_ITEM, DELETE_ITEM } from '../types';
 
 export default (state, action) => {
   switch (action.type) {
-    case GET_ALL_DATA:
+    case GET_ITEMS:
       return {
         ...state,
         technologyData: action.payload,
+      };
+
+    case ADD_ITEM:
+      return {
+        ...state,
+        technologyData: [...state.technologyData, action.payload],
+      };
+
+    case UPDATE_ITEM:
+      return {
+        ...state,
+        technologyData: [...state.technologyData, action.payload],
+      };
+
+    case DELETE_ITEM:
+      return {
+        ...state,
+        technologyData: state.technologyData.filter(
+          (item) => item.id !== action.payload
+        ),
       };
 
     default:
